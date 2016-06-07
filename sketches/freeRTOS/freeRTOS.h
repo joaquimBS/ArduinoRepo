3296ea2d58b6bd0bdc79eeba0ecc8dcdb9711db1
Joaquim Barrera <quim.barrera@gmail.com>
2016-06-08 00:27:45 +0200

    [2016.06.08]

--- sketches/freeRTOS/freeRTOS.h
+++ sketches/freeRTOS/freeRTOS.h
@@ -2,13 +2,15 @@
 #define FREERTOS_H
 
 // Custom Arduino libaries
-#include <DHT.h>
+#include <dht.h>
 #include "RTClib.h"			// https://github.com/adafruit/RTClib
 // #include "IRremote.h"		// https://github.com/z3t0/Arduino-IRremote
 #include <Arduino_FreeRTOS.h>
+#include "semphr.h"  // add the FreeRTOS functions for Semaphores (or Flags).
+#include <MemoryFree.h>
+
 #include <avr/power.h>
 #include <avr/sleep.h>
-// #include "semphr.h"  // add the FreeRTOS functions for Semaphores (or Flags).
 
 // Pinout
 // #define PIN0  0		// Rx
@@ -18,7 +20,7 @@
 #define DHTPIN			4
 // #define DHT22_VCC 		5
 // #define PIN_BTN_LESS	6
-// #define RTC_VCC			7
+#define OLED_VCC		7
 // #define RELE_PIN		8
 // #define INFO_LED		9
 // #define PIN10			10
--- sketches/freeRTOS/freeRTOS.ino
+++ sketches/freeRTOS/freeRTOS.ino
@@ -3,16 +3,91 @@
 
 // Declare a mutex Semaphore Handle which we will use to manage the Serial Port.
 // It will be used to ensure only only one Task is accessing this resource at any time.
-// SemaphoreHandle_t xSerialSemaphore;
+SemaphoreHandle_t xMySemaphore;
+SemaphoreHandle_t xWireSemaphore;
 
 unsigned long tick = 0;
+unsigned long dReadTime = 0;
 
 // define two Tasks for DigitalRead & AnalogRead
-void TaskDigitalRead( void *pvParameters );
+void TaskFastPeriod( void *pvParameters );
 void TaskTemperatureRead( void *pvParameters );
+void TaskTimeRead( void *pvParameters );
 
-DHT dht(DHTPIN, DHTTYPE);
+#define FREQ_1000_MS  1000
+#define FREQ_500_MS   500
+#define FREQ_100_MS   100
+
+dht DHT;
 SSD1306AsciiAvrI2c oled;
+RTC_DS1307 rtc;
+DateTime now;
+
+void InitRtc(int timeout_ms)
+{
+  bool isRTCInit = false;
+
+  rtc.begin();
+
+  unsigned long t0 = millis();
+  
+
+  while(!isRTCInit && ((millis() - t0) < timeout_ms) ) {
+    if(rtc.isrunning())
+      isRTCInit = true;
+    else {
+      delay(25);
+      Serial.println(millis());
+    }
+  }
+
+  if(isRTCInit) {
+    Serial.println("RTC\tOK!");
+    _printDateTime();
+  }
+  else {
+    Serial.println("RTC\tFail!");
+  }
+
+  // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+}
+
+
+void _printDateTime()
+{
+  // if(isRTCInit) {
+    now = rtc.now();
+
+    Serial.print(now.year(), DEC);
+    Serial.print('/');
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.day(), DEC);
+    Serial.print(' ');
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+    Serial.println();
+
+    Serial.println(now.unixtime());
+
+    // delay(100);
+  // }
+}
+
+void InitOled()
+{
+  pinMode(OLED_VCC, OUTPUT);
+  digitalWrite(OLED_VCC, LOW);
+
+  delay(100);
+
+  oled.begin(&Adafruit128x64, I2C_ADDRESS);
+  oled.setFont(Stang5x7);
+  oled.clear();
+}
 
 // the setup function runs once when you press reset or power the board
 void setup() {
@@ -20,43 +95,53 @@ void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(115200);
 
-  // oled.begin(&Adafruit128x64, I2C_ADDRESS);
-  // oled.setFont(System5x7);
-  // oled.clear();
-
-  dht.begin();
-
-  
-
+  InitOled();
+  // delay(100);
+  // InitRtc(100);
 
   // Semaphores are useful to stop a Task proceeding, where it should be paused to wait,
   // because it is sharing a resource, such as the Serial port.
   // Semaphores should only be used whilst the scheduler is running, but we can set it up here.
-  // if ( xSerialSemaphore == NULL )  // Check to confirm that the Serial Semaphore has not already been created.
-  // {
-  //   xSerialSemaphore = xSemaphoreCreateMutex();  // Create a mutex semaphore we will use to manage the Serial Port
-  //   if ( ( xSerialSemaphore ) != NULL )
-  //     xSemaphoreGive( ( xSerialSemaphore ) );  // Make the Serial Port available for use, by "Giving" the Semaphore.
-  // }
+  if ( xMySemaphore == NULL )  // Check to confirm that the Serial Semaphore has not already been created.
+  {
+    xMySemaphore = xSemaphoreCreateBinary();  // Create a mutex semaphore we will use to manage the Serial Port
+    if ( ( xMySemaphore ) != NULL )
+      xSemaphoreTake( xMySemaphore, ( TickType_t ) 0 );
+  }
+
+  if ( xWireSemaphore == NULL )  // Check to confirm that the Serial Semaphore has not already been created.
+  {
+    xWireSemaphore = xSemaphoreCreateMutex();  // Create a mutex semaphore we will use to manage the Serial Port
+    if ( ( xWireSemaphore ) != NULL )
+      xSemaphoreGive( xWireSemaphore );
+  }
 
   pinMode(INFO_LED, OUTPUT);
   
 
   // Now set up two Tasks to run independently.
-  // xTaskCreate(
-  //   TaskDigitalRead
-  //   ,  (const portCHAR *)"DR1"  // A name just for humans
-  //   ,  128  // This stack size can be checked & adjusted by reading the Stack Highwater
-  //   ,  NULL
-  //   ,  1  // Priority, with 1 being the highest, and 4 being the lowest.
-  //   ,  NULL );
+  xTaskCreate(
+    TaskFastPeriod
+    ,  (const portCHAR *)"DR1"  // A name just for humans
+    ,  256  // This stack size can be checked & adjusted by reading the Stack Highwater
+    ,  NULL
+    ,  2  // Priority, with 1 being the highest, and 4 being the lowest.
+    ,  NULL );
 
   xTaskCreate(
     TaskTemperatureRead
     ,  (const portCHAR *) "TR1"
-    ,  128  // Stack size
+    ,  101   // Stack size
     ,  NULL
-    ,  2  // Priority
+    ,  1  // Priority
+    ,  NULL );
+
+  xTaskCreate(
+    TaskTimeRead
+    ,  (const portCHAR *) "TR2"
+    ,  100  // Stack size
+    ,  NULL
+    ,  1  // Priority
     ,  NULL );
 
   // Now the Task scheduler, which takes over control of scheduling individual Tasks, is automatically started.
@@ -65,210 +150,102 @@ void setup() {
 
 void loop()
 {
-  // Empty. Things are done in Tasks.
-  // tick++;
-  // digitalWrite(13, LOW);
-  // set_sleep_mode( SLEEP_MODE_PWR_DOWN );
-  // sleep_enable();
-  // sleep_mode();
+  tick++;
+  digitalWrite(13, LOW);
+
+  set_sleep_mode( SLEEP_MODE_PWR_DOWN );
+  sleep_enable();
+  sleep_mode();
 }
 
 /*--------------------------------------------------*/
 /*---------------------- Tasks ---------------------*/
 /*--------------------------------------------------*/
 
+char buff[32];
 
-// void TaskDigitalRead( void *pvParameters __attribute__((unused)) )  // This is a Task.
-// {
-//   /*
-//     DigitalReadSerial
-//     Reads a digital input on pin 2, prints the result to the serial monitor
+void TaskFastPeriod( void *pvParameters __attribute__((unused)) )  // This is a Task.
+{
+  // char buff[32];
+  pinMode(PIN_BTN_YLW, INPUT_PULLUP);
+  int ut = 0;
+  int buttonState = digitalRead(PIN_BTN_YLW);
 
-//     This example code is in the public domain.
-//   */
+  for (;;) // A Task shall never return or exit.
+  {
+    // read the input pin:
+    buttonState = digitalRead(PIN_BTN_YLW);
+    digitalWrite(INFO_LED, !buttonState);
 
-//   // digital pin 2 has a pushbutton attached to it. Give it a name:
-//   // make the pushbutton's pin an input:
-//   pinMode(PIN_BTN_YLW, INPUT_PULLUP);
+    if ( xSemaphoreTake( xMySemaphore, ( TickType_t ) 0 ) == pdTRUE )
+    {
+      if ( xSemaphoreTake( xWireSemaphore, ( TickType_t ) 10 ) == pdTRUE ) {
+        
+        ut = now.unixtime();
 
-//   for (;;) // A Task shall never return or exit.
-//   {
-//     // read the input pin:
-//     int buttonState = digitalRead(PIN_BTN_YLW);
-//     digitalWrite(INFO_LED, !buttonState);
+        // Serial.println(buff);
+        oled.home();
+        oled.set2X();
 
-//     // if(!buttonState) {
-//     // Serial.print(buttonState);
-//     vTaskDelay(50 / portTICK_PERIOD_MS);
+        sprintf(buff, "%02d:%02d:%02d", now.hour(), now.minute(), now.second());
+        oled.println(buff);
 
-//     // }
+        sprintf(buff, "%d.%d C", int(DHT.temperature*10)/10, int(DHT.temperature*10)%10);
+        oled.println(buff);
 
-//     // See if we can obtain or "Take" the Serial Semaphore.
-//     // If the semaphore is not available, wait 5 ticks of the Scheduler to see if it becomes free.
-//     // if ( xSemaphoreTake( xSerialSemaphore, ( TickType_t ) 5 ) == pdTRUE )
-//     // {
-//       // We were able to obtain or "Take" the semaphore and can now access the shared resource.
-//       // We want to have the Serial Port for us alone, as it takes some time to print,
-//       // so we don't want it getting stolen during the middle of a conversion.
-//       // print out the state of the button:
-      
+        sprintf(buff, "%d.%d Hum.", int(DHT.humidity*10)/10, int(DHT.humidity*10)%10);
+        oled.println(buff);
 
-//       // xSemaphoreGive( xSerialSemaphore ); // Now free or "Give" the Serial Port for others.
-//     // }
+        oled.set1X();
+        oled.println(dReadTime);
+        oled.println(tick);
 
-//     // vTaskDelay(1);  // one tick delay (15ms) in between reads for stability
-//   }
-// }
+        xSemaphoreGive( xWireSemaphore );
+      }
+    }
+    vTaskDelay(FREQ_100_MS / portTICK_PERIOD_MS);
+  }
+}
 
-#define FREQ_MS  2000
 void TaskTemperatureRead( void *pvParameters __attribute__((unused)) )  // This is a Task.
 {
   TickType_t xLastWakeTime = xTaskGetTickCount ();
-  TickType_t xFrequency = FREQ_MS / portTICK_PERIOD_MS ;
- 
+  TickType_t xFrequency = FREQ_500_MS / portTICK_PERIOD_MS ;
+  
   for (;;)
   {
     vTaskDelayUntil( &xLastWakeTime, xFrequency );
 
-    // digitalWrite(13, HIGH);
+    digitalWrite(13, HIGH);
+    // Serial.println(F("TaskTemperatureRead"));
 
+    dReadTime = micros();
+    
     // taskENTER_CRITICAL();
-
-    float t = dht.readTemperature();
-    Serial.println(t);
-    
-    // oled.home();
-    // oled.println(dht.readTemperature());
-    // oled.println(tick);
+    while(DHT.read22(DHTPIN) != DHTLIB_OK)
+      vTaskDelay(portTICK_PERIOD_MS);
     // taskEXIT_CRITICAL();
-
-    // delay(50);
-    // old_millis = millis();
-
-    
-    
-
-    // See if we can obtain or "Take" the Serial Semaphore.
-    // If the semaphore is not available, wait 5 ticks of the Scheduler to see if it becomes free.
-    // if ( xSemaphoreTake( xSerialSemaphore, ( TickType_t ) 5 ) == pdTRUE )
-    // {
-      // We were able to obtain or "Take" the semaphore and can now access the shared resource.
-      // We want to have the Serial Port for us alone, as it takes some time to print,
-      // so we don't want it getting stolen during the middle of a conversion.
-      // print out the value you read:
-      // Serial.println(millis() - old_millis);
       
+    dReadTime = micros() - dReadTime;
+    digitalWrite(13, LOW);
 
-      // Serial.println(xFrequency);
-      // Serial.println(portTICK_PERIOD_MS);
-      // Serial.println(xLastWakeTime);
-      
-      // Serial.println(sensorValue);
-
-      // xSemaphoreGive( xSerialSemaphore ); // Now free or "Give" the Serial Port for others.
-    // }
-
-    // diff_millis = FREQ_MS - (millis() - old_millis);
-    // Serial.println(diff_millis);
-    // xFrequency = (FREQ_MS + diff_millis) / portTICK_PERIOD_MS ;
-    // vTaskDelay(1);  // one tick delay (15ms) in between reads for stability
+    xSemaphoreGive( xMySemaphore ); // Now free or "Give" the Serial Port for others.
   }
 }
 
+void TaskTimeRead( void *pvParameters __attribute__((unused)) )  // This is a Task.
+{
+  TickType_t xLastWakeTime = xTaskGetTickCount ();
+  TickType_t xFrequency = FREQ_500_MS / portTICK_PERIOD_MS ;
+  bool state = true;
 
+  for (;;)
+  {
+    vTaskDelayUntil( &xLastWakeTime, xFrequency );
 
-// /******************************************************************************
-// FreeRTOS timed blink example
-
-// Byron Jacquot @ SparkFun Electronics>
-
-// Porting Marshall Taylor's out-of-synch blinker example
-// to an RTOS, to show off basic features.
-
-// Resources:
-// Requires Bill Greiman's port of Free RTOS to Arduino
-// https://github.com/greiman/FreeRTOS-Arduino
-
-// Development environment specifics:
-// Arduino 1.6.5
-// SparkFun RebBoard with additional LED on pin 12.
-
-// This code is released under the [MIT License](http://opensource.org/licenses/MIT).
-// Distributed as-is; no warranty is given.
-// ******************************************************************************/
-
-// #include <Arduino_FreeRTOS.h>
-
-// // define two tasks for Blink & AnalogRead
-// void TaskBlink( void *pvParameters );
-// void TaskAnalogRead( void *pvParameters );
-
-// void setup() 
-// {
-
-//   // Now set up two tasks to run independently.
-//   xTaskCreate(
-//     TaskBlink
-//     ,  (const portCHAR *)"Blink"   // A name just for humans
-//     ,  128  // This stack size can be checked & adjusted by reading the Stack Highwater
-//     ,  NULL
-//     ,  2  // Priority, with 1 being the highest, and 4 being the lowest.
-//     ,  NULL );
-
-//   xTaskCreate(
-//     TaskAnalogRead
-//     ,  (const portCHAR *) "AnalogRead"
-//     ,  128  // Stack size
-//     ,  NULL
-//     ,  1  // Priority
-//     ,  NULL );
-
-//   // Now the task scheduler, which takes over control of scheduling individual tasks, is automatically started.
-// }
-
-// //------------------------------------------------------------------------------
-// // WARNING idle loop has a very small stack (configMINIMAL_STACK_SIZE)
-// // loop must never block
-// void loop() 
-// {
-//   // Empty. Things are done in Tasks.
-// }
-
-// /*--------------------------------------------------*/
-// /*---------------------- Tasks ---------------------*/
-// /*--------------------------------------------------*/
-
-// void TaskBlink(void *pvParameters)  // This is a task.
-// {
-//   (void) pvParameters;
-
-//   // initialize digital pin 13 as an output.
-//   pinMode(13, OUTPUT);
-
-//   for (;;) // A Task shall never return or exit.
-//   {
-//     digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-//     vTaskDelay( 1000 / portTICK_PERIOD_MS ); // wait for one second
-//     digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-//     vTaskDelay( 1000 / portTICK_PERIOD_MS ); // wait for one second
-//   }
-// }
-
-// void TaskAnalogRead(void *pvParameters)  // This is a task.
-// {
-//   (void) pvParameters;
-
-//   // initialize serial communication at 9600 bits per second:
-//   Serial.begin(9600);
-
-//   for (;;)
-//   {
-//     // read the input on analog pin 0:
-//     int sensorValue = analogRead(A0);
-//     // print out the value you read:
-//     Serial.println(sensorValue);
-//     vTaskDelay(1);  // one tick delay (30ms) in between reads for stability
-//   }
-// }
-
-///////////////////////////////////////////////////////////////////////////////////
+    if ( xSemaphoreTake( xWireSemaphore, ( TickType_t ) 10 ) == pdTRUE ) {
+      now = rtc.now();
+      xSemaphoreGive( xWireSemaphore );
+    }
+  }
+}
